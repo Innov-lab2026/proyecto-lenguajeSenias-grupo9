1084dbc16e9015973cce724941477c6bf49e3f76
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-01 10:38:22 -0300

    added data/teamdata.js for landingpage

---
+++ landing-page/src/data/TeamData.js
@@ -0,0 +1,47 @@
+export const teamData = {
+    data: {
+        title: "Data Analytic",
+        description: "",
+        members: [
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" }
+        ]
+    },
+    uxui: {
+        title: "Diseño UX/UI",
+        description: "",
+        members: [
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" }
+        ]
+    },
+    frontend: {
+        title: "Frontend",
+        description: "",
+        members: [
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" }
+        ]
+    },
+    backend: {
+        title: "Backend",
+        description: "",
+        members: [
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" }
+        ]
+    },
+    qa: {
+        title: "Testing QA",
+        description: "",
+        members: [
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" },
+            { name: "", linkedin: "", github: "" }
+        ]
+    }
+};
