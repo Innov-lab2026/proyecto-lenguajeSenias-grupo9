aa8bc0bf3e906a602996e1080af534aec09eb621
Sebas de la Torre <lucasebasdlt@gmail.com>
2026-06-02 12:30:34 -0300

    feat/landing:refactor header & hero section
    components: create button-download, navbar & title

---
+++ landing-page/src/components/navbar/index.tsx
@@ -0,0 +1,32 @@
+import ButtonDownload from "../button-dowload";
+
+export default function Navbar() {
+  return (
+    <nav
+      className="
+    flex
+    items-center
+    gap-4 md:gap-6 lg:gap-8
+    text-base lg:text-lg
+  "
+    >
+      <a href="#home" className="header-link">
+        Inicio
+      </a>
+
+      <a href="#demo" className="header-link">
+        Demo
+      </a>
+
+      <a href="#project" className="header-link">
+        Proyecto
+      </a>
+
+      <a href="#team" className="header-link">
+        Equipo
+      </a>
+
+      <ButtonDownload />
+    </nav>
+  );
+}
---
+++ landing-page/src/components/tittle/index.tsx
@@ -0,0 +1,3 @@
+export default function Tittle() {
+  return <p className="font-semibold text-2xl lg:text-3xl">LSA APP</p>;
+}
