cba4dab802f94d56fba7b8c10421a49dce785f14
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-09 20:21:01 -0300

    added ProjectHeader components for title and paragraphs, and ProjectFeatures for cards with features data

--- landing-page/src/components/project/ProjectFeatureCard.tsx
+++ landing-page/src/components/project/ProjectFeatureCard.tsx
@@ -1,12 +1,11 @@
-export default function ProjectFeatureCard({title, description, image}: {title: string, description: string, image: string}) {
+export default function ProjectFeatureCard({ title, description, image }: { title: string, description: string, image: string }) {
     return (
-        <div className="flex flex-col gap-4">
-            <div className="w-16 h-16  shrink-0" />
-            <div>
-                <img src={image} alt={title} />
-                <h2 className="font-medium mb-2">{title}</h2>
-                <p>{description}</p>
-            </div>
+
+        <div className="flex flex-col gap-2 max-w-71 mx-auto">
+            <img src={image} alt={title} className="w-80" />
+            <h2 className="font-extrabold text-[24px] mb-2 whitespace-nowrap">{title}</h2>
+            <p className="text-[20px]">{description}</p>
         </div>
+
     )
 }
--- landing-page/src/sections/Project.tsx
+++ landing-page/src/sections/Project.tsx
@@ -1,76 +1,16 @@
 import SectionLayout from '../layout/SectionLayout'
+import ProjectFeatures from '../components/project/ProjectFeatures'
+import ProjectHeader from '../components/project/ProjectHeader'
+
 
 export default function Project() {
     return (
         <SectionLayout
             id="project"
         >
-            <div className="max-w-6xl mx-auto px-6 lg:px-16 my-10">
-
-                <h1 className="text-4xl text-center font-bold text-[56px] mb-16 pt-10">
-                    ¿Qué es CarpiSeñas?
-                </h1>
-
-                <p className="text-center text-[30px] leading-relaxed max-w-5xl mx-auto">
-                    Carpiseñas es una App pensada para acercar el Lenguaje de 
-                    Señas Argentina a personas oyentes de manera accesible, 
-                    divertida e inclusiva.
-                </p>
-            </div>
-
-            <div className=" py-4">
-                <div className=" max-w-6xl mx-auto px-6 lg:px-16">
-
-                    <article className="grid md:grid-cols-3 gap-10 ">
-
-                        <div className="flex gap-4">
-                            <div className="w-16 h-16 bg-gray-300 shrink-0" />
-
-                            <div>
-                                <h2 className="font-medium mb-2">
-                                    Lecciones cortas
-                                </h2>
-
-                                <p className="text-sm text-gray-700 leading-relaxed">
-                                    Contenido modular de 5 minutos diseñado para fijar conceptos
-                                    sin abrumarte.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="flex gap-4">
-                            <div className="w-16 h-16 bg-gray-300 shrink-0" />
-
-                            <div>
-                                <h2 className="font-medium mb-2">
-                                    Aprendizaje interactivo
-                                </h2>
-
-                                <p className="text-sm text-gray-700 leading-relaxed">
-                                    Aprende jugando con ejercicios dinámicos y desafíos
-                                    divertidos.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="flex gap-4">
-                            <div className="w-16 h-16 bg-gray-300 shrink-0" />
-
-                            <div>
-                                <h2 className="font-medium mb-2">
-                                    Progreso motivador
-                                </h2>
-
-                                <p className="text-sm text-gray-700 leading-relaxed">
-                                    Sumá puntos, completá niveles y desbloqueá logros mientras
-                                    aprendés.
-                                </p>
-                            </div>
-                        </div>
-
-                    </article>
-                </div>
-            </div>
+            <ProjectHeader />
+            <ProjectFeatures />
+            
         </SectionLayout>
     )
 }
