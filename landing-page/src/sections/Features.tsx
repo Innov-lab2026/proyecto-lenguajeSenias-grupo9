4d743f71bd697760cc21b61abbb2a4d16c121932
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-10 17:56:12 -0300

    Restructuring of sections according to figma design

--- landing-page/src/sections/Features.tsx
+++ landing-page/src/sections/Features.tsx
@@ -3,8 +3,7 @@ import SectionLayout from '../layout/SectionLayout'
 export default function Features() {
     return (
         <SectionLayout
-            id="features"
-            className="bg-gray-100">
+            id="features">
 
             <div className="flex flex-col items-center justify-center gap-6 p-8 text-center h-full">
                 <h1 className="text-4xl font-bold">Aprende paso a paso</h1>
