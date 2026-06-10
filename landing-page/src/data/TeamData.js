c0f78719d7a6b201bc832fd6c1ee857673175af2
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-10 19:42:42 -0300

    updated TeamData.js

--- landing-page/src/data/TeamData.js
+++ landing-page/src/data/TeamData.js
@@ -3,45 +3,45 @@ export const teamData = {
         title: "Data Analytic",
         description: "",
         members: [
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" }
+            { name: "Matias De Vivo", linkedin: "", github: "" },
+            { name: "Inés Abarrategui", linkedin: "", github: "" },
+            { name: "Julian Outeyral", linkedin: "", github: "" }
         ]
     },
     uxui: {
         title: "Diseño UX/UI",
         description: "",
         members: [
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" }
+            { name: "Sol Diessler", linkedin: "", github: "" },
+            { name: "Belén Coronel", linkedin: "", github: "" },
+            { name: "Karina Rosa", linkedin: "", github: "" }
         ]
     },
     frontend: {
         title: "Frontend",
         description: "",
         members: [
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" }
+            { name: "Ezequiel Oliver", linkedin: "", github: "" },
+            { name: "María Cerpa", linkedin: "", github: "" },
+            { name: "Juan Martinez", linkedin: "", github: "" }
         ]
     },
     backend: {
         title: "Backend",
         description: "",
         members: [
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" }
+            { name: "Elisa Aroya", linkedin: "", github: "" },
+            { name: "Araceli Fernández", linkedin: "", github: "" },
+            { name: "Mauricio Soto", linkedin: "", github: "" }
         ]
     },
     qa: {
         title: "Testing QA",
         description: "",
         members: [
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" },
-            { name: "", linkedin: "", github: "" }
+            { name: "María Martin", linkedin: "", github: "" },
+            { name: "Julian Salazar", linkedin: "", github: "" },
+            // { name: "", linkedin: "", github: "" }
         ]
     }
 };
