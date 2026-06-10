6baeac93b1b41e24c1e38c8aa16ac0fb13aed84a
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-10 18:26:50 -0300

    Added and implemented FeatureCard component

--- landing-page/src/sections/Features.tsx
+++ landing-page/src/sections/Features.tsx
@@ -1,3 +1,7 @@
+import FeatureCard from '../components/features/FeatureCard'
+import letraA from '../assets/images/letra-A.png'
+import manito from '../assets/images/manito.svg'
+
 import SectionLayout from '../layout/SectionLayout'
 
 export default function Features() {
@@ -5,8 +9,20 @@ export default function Features() {
         <SectionLayout
             id="features">
 
-            <div className="flex flex-col items-center justify-center gap-6 p-8 text-center h-full">
-                <h1 className="text-4xl font-bold">Aprende paso a paso</h1>
+            <div className="flex flex-col items-center justify-center gap-6 pt-22 text-center h-full">
+                <h2 className="text-[56px] pb-12">Aprende paso a paso</h2>
+                <div className="flex flex-col md:flex-row gap-6">
+                    <FeatureCard
+                        title="Abecedarios LSA"
+                        description="Aprende las letras del abecedario con ejercicios interactivos. Puedes hacerlo por letra o ingresando la palabra que te interese."
+                        image={letraA}
+                        className="w-120" />
+                    <FeatureCard
+                        title="Palabras básicas"
+                        description="Reconoce y practica palabras del vocabulario cotidiano mediante desafios."
+                        image={manito}
+                        className="w-70" />
+                </div>
             </div>
 
         </SectionLayout>
