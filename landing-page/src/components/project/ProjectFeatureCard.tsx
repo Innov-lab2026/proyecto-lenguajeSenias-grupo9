090da77d05e6e85c1f2534ab645fc42946647bca
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-14 19:13:15 -0300

    Improvements to the responsive design of the Hero and Project sections

--- landing-page/src/components/project/ProjectFeatureCard.tsx
+++ landing-page/src/components/project/ProjectFeatureCard.tsx
@@ -1,10 +1,23 @@
 export default function ProjectFeatureCard({ title, description, image }: { title: string, description: string, image: string }) {
     return (
 
-        <div className="flex flex-col gap-2 max-w-71 mx-auto">
-            <img src={image} alt={title} className="w-80" />
-            <h2 className="font-extrabold text-[24px] mb-2 whitespace-nowrap">{title}</h2>
-            <p className="text-[20px]">{description}</p>
+        <div className="flex flex-col gap-0 md:gap-2 max-w-60 lg:max-w-71 mx-auto">
+
+            <div className="flex items-center w-60 lg:w-80 h-60 lg:h-80">
+                <img src={image} alt={title} className="w-full" />
+            </div>
+
+            <h2 className="font-extrabold 
+                           text-xl lg:text-2xl 
+                           text-center md:text-left 
+                           mb-2 whitespace-nowrap">
+                {title}
+            </h2>
+
+            <p className="text-lg lg:text-xl 
+                          text-center md:text-left">
+                {description}
+            </p>
         </div>
 
     )
--- landing-page/src/components/project/ProjectHeader.tsx
+++ landing-page/src/components/project/ProjectHeader.tsx
@@ -2,11 +2,13 @@ export default function ProjectHeader() {
     return (
         <div className="max-w-6xl mx-auto px-6 lg:px-16 my-10">
 
-            <h1 className="text-4xl text-center font-bold text-[56px] mb-10 pt-10">
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl 
+                           text-center font-bold mb-10 pt-10">
                 ¿Qué es CarpiSeñas?
             </h1>
 
-            <p className="text-center text-[30px] leading-relaxed max-w-5xl mx-auto">
+            <p className="text-xl sm:text-2xl lg:text-3xl text-center 
+                          leading-relaxed max-w-5xl mx-auto">
                 Carpiseñas es una App pensada para acercar el Lenguaje de
                 Señas Argentina a personas oyentes de manera accesible,
                 divertida e inclusiva.
