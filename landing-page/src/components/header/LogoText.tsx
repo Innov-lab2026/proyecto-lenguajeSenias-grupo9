06ad09b6dc2b7773be40a3a083c80861f3407a97
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-06-17 20:49:34 -0300

    Added 'strict: true' to tsconfig.app.json; fixed texts sizes

--- landing-page/src/components/header/LogoText.tsx
+++ landing-page/src/components/header/LogoText.tsx
@@ -2,7 +2,7 @@ export default function LogoText() {
   return (
     <span className="font-extrabold text-3xl md:text-4xl lg:text-5xl z-30">
       <span className="text-success">Carpi</span>
-      <span className="text-foreground z-70">Señas</span>
+      <span className="text-foreground z-30">Señas</span>
     </span>
   );
 }
--- landing-page/src/sections/Footer.tsx
+++ landing-page/src/sections/Footer.tsx
@@ -3,11 +3,11 @@ export default function Footer() {
         <footer className="flex flex-col sm:flex-row items-center justify-between 
                             gap-4 p-4 bg-surface rounded-t-full h-28 px-20">
 
-            <p className="font-bold text-[28px]">
+            <p className="font-bold text-2xl">
                 CarpiSeñas
             </p>
 
-            <p className="text-[12px]">
+            <p className="text-xs text-center sm:text-right">
                 &copy; 2026 Carpiseñas. Todos los derechos reservados.
             </p>
 
