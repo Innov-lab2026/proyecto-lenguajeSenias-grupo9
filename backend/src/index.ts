4714d3acc0166cacb657e11316e2555b1e4e09e8
Araceli Fernandez <aracelifernandez.3006@gmail.com>
2026-06-26 20:39:17 -0300

    configuracion para deploy en vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,8 +20,10 @@ app.get('/', (req, res) => {
 
 setupSwagger(app)
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
 export default app
